58cd90857ac3c3d51d5e3461c69cded058e0eb5b
Amal Jossy <amaljossy1@gmail.com>
2019-01-07 11:51:03 +0530

    Initial Commit

---
+++ Assets/Scripts/ChestBehaviour.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Collider))]
+public class ChestBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject chest, lid;
+    static private Vector3 up = new Vector3(0, .5f, 0);
+    static private Vector3 down = new Vector3(0, -.5f, 0);
+    void Start()
+    {
+    }
+    public void SetGazedAt(bool gazedAt)
+    {
+        //hint for interaction
+        if (gazedAt)
+            lid.transform.Translate(up);
+        else
+            lid.transform.Translate(down);
+    }
+    public void Reset() { }
+    public void Recenter()
+    {
+#if !UNITY_EDITOR
+        GvrCardboardHelpers.Recenter();
+#else
+        if (GvrEditorEmulator.Instance != null)
+        {
+            GvrEditorEmulator.Instance.Recenter();
+        }
+#endif  // !UNITY_EDITOR
+    }
+    // Update is called once per frame
+    public void openLid()
+    {
+        //open chest lid
+        lid.transform.Translate(Vector3.up);
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Getting started
+1. Clone this repository
+1. Download and import [GoogleVR SDK](https://developers.google.com/vr/develop/unity/download)
+1. Download and import [LowPoly Environment Pack](https://assetstore.unity.com/packages/3d/environments/landscapes/lowpoly-environment-pack-99479)
+1. Download and import [Low Poly treasure chest pack](https://assetstore.unity.com/packages/3d/props/low-poly-treasure-chest-pack-126963)
+1. `Player settings > XR settings`
+    1. Virtual Reality supported
+    1. Add `Cardboard` under SDKs
+1. Configure your build settings and build the forest scene
